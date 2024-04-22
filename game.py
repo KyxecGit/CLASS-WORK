d8a017e1bd8775a49827f7166eb487b3c7372033
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-04-22 16:02:18 +0600

    Update game.py

--- game.py
+++ game.py
@@ -6,11 +6,9 @@ class GameSprite(sprite.Sprite):
     #конструктор класса
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
- 
         # каждый спрайт должен хранить свойство image - изображение
         self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
- 
         # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
         self.rect.x = player_x

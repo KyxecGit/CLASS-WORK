bf13be7993f00a8b07a89a1353a339fddee253d9
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-04-28 08:51:26 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,23 @@
-#Создай собственный Шутер!
-from random import randint
-from typing import Any
 from pygame import *
+from random import randint
+
+#фоновая музыка
+#mixer.init()
+#mixer.music.load('space.ogg')
+#mixer.music.play()
+#fire_sound = mixer.Sound('fire.ogg')
+
+#шрифты и надписи
+font.init()
+font2 = font.Font(None, 36)
+
+# нам нужны такие картинки:
+img_back = "galaxy.jpg" # фон игры
+img_hero = "rocket.png" # герой
+img_enemy = "ufo.png" # враг
+
+score = 0 # сбито кораблей
+lost = 0 # пропущено кораблей
 
 # класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
@@ -25,50 +41,73 @@ class GameSprite(sprite.Sprite):
 
 # класс главного игрока
 class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT]:
+        if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT]:
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
+  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    def fire(self):
+        pass
 
-# класс врагов
+# класс спрайта-врага   
 class Enemy(GameSprite):
+    # движение врага
     def update(self):
         self.rect.y += self.speed
-        if self.rect.y > window_height:
+        global lost
+        # исчезает, если дойдет до края экрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
             self.rect.y = 0
-            self.rect.x = randint(0,700)
+            lost = lost + 1
 
-#Создаем персонажей
-ship  = Player('rocket.png',300,380,70,120,10)
+# Создаем окошко
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
 
-enemys = sprite.Group()
-for i in range(10):
-    enemy = Enemy('ufo.png',randint(0,700),0,100,50,randint(5,10))
-    enemys.add(enemy)
+# создаем спрайты
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
-#Создаем окно
-window_width = 700
-window_height = 500
-window = display.set_mode((window_width,window_height))
-background = transform.scale(image.load('galaxy.jpg'),(window_width,window_height))
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monsters.add(monster)
 
-#Игровой цикл
-game = True
-while game:
-   
+# переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+# Основной цикл игры:
+run = True # флаг сбрасывается кнопкой закрытия окна
+while run:
+    # событие нажатия на кнопку Закрыть
     for e in event.get():
         if e.type == QUIT:
-            game = False
+            run = False
 
-    window.blit(background,(0,0))
+    if not finish:
+        # обновляем фон
+        window.blit(background,(0,0))
 
-    ship.reset()
-    ship.update()
+        # пишем текст на экране
+        text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
 
-    enemys.draw(window)
-    enemys.update()
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
 
-    display.update()
-    time.delay(20)
+        # производим движения спрайтов
+        ship.update()
+        monsters.update()
+
+        # обновляем их в новом местоположении при каждой итерации цикла
+        ship.reset()
+        monsters.draw(window)
+
+        display.update()
+    # цикл срабатывает каждую 0.05 секунд
+    time.delay(50)

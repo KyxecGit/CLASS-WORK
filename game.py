6d88df29b56ea998f2eeec1384fa6a9d885d66a3
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-04-29 14:23:50 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,12 +1,11 @@
-from typing import Any
 from pygame import *
 from random import randint
 
 #фоновая музыка
-mixer.init()
-mixer.music.load('space.ogg')
-mixer.music.play()
-fire_sound = mixer.Sound('fire.ogg')
+#mixer.init()
+#mixer.music.load('space.ogg')
+#mixer.music.play()
+#fire_sound = mixer.Sound('fire.ogg')
 
 #шрифты и надписи
 font.init()
@@ -51,8 +50,7 @@ class Player(GameSprite):
             self.rect.x += self.speed
   # метод "выстрел" (используем место игрока, чтобы создать там пулю)
     def fire(self):
-        bullet = Bullet('bullet.png',self.rect.centerx,self.rect.top,15,20,20)
-        bullets.add(bullet)
+        pass
 
 # класс спрайта-врага   
 class Enemy(GameSprite):
@@ -66,12 +64,6 @@ class Enemy(GameSprite):
             self.rect.y = 0
             lost = lost + 1
 
-class Bullet(GameSprite):
-    def update(self):
-        self.rect.y -= self.speed
-        if self.rect.y < 0:
-            self.kill()
-
 # Создаем окошко
 win_width = 700
 win_height = 500
@@ -82,7 +74,6 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
 # создаем спрайты
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
-bullets = sprite.Group()
 monsters = sprite.Group()
 for i in range(1, 6):
     monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
@@ -97,12 +88,6 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-        
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                fire_sound.play()
-                ship.fire()
-
 
     if not finish:
         # обновляем фон
@@ -118,34 +103,11 @@ while run:
         # производим движения спрайтов
         ship.update()
         monsters.update()
-        bullets.update()
 
         # обновляем их в новом местоположении при каждой итерации цикла
         ship.reset()
         monsters.draw(window)
-        bullets.draw(window)
-
-        if sprite.groupcollide(monsters,bullets,True,True):
-            score += 1
-            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-            monsters.add(monster)
-
-        if lost >= 5 or sprite.spritecollide(ship,monsters,True):
-            finish = True
-            lose = font.Font(None,100).render("LOSE", 1, (255, 0, 0))
-            window.blit(lose, (250, 250))
-
-        if score > 10:
-            finish = True
-            win = font.Font(None,100).render("WIN", 1, (0, 255, 0))
-            window.blit(win, (250, 250))
 
         display.update()
-
-    else:
-        finish = False
-        lost = 0
-        score = 0
-        time.delay(5000)
     # цикл срабатывает каждую 0.05 секунд
     time.delay(50)

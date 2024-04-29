0dd5e2c54fdddcc88b94d442ec50e553157cfa10
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-04-29 15:05:57 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from typing import Any
 from pygame import *
 from random import randint
 
@@ -50,7 +51,12 @@ class Player(GameSprite):
             self.rect.x += self.speed
   # метод "выстрел" (используем место игрока, чтобы создать там пулю)
     def fire(self):
-        pass
+        bullet = Bullet('bullet.png',self.rect.centerx,self.rect.top,15,20,20)
+        bullets.add(bullet)
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
 
 # класс спрайта-врага   
 class Enemy(GameSprite):
@@ -74,6 +80,7 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
 # создаем спрайты
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
+bullets = sprite.Group()
 monsters = sprite.Group()
 for i in range(1, 6):
     monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
@@ -88,6 +95,10 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                #fire_sound.play()
+                ship.fire()
 
     if not finish:
         # обновляем фон
@@ -103,11 +114,17 @@ while run:
         # производим движения спрайтов
         ship.update()
         monsters.update()
+        bullets.update()
 
         # обновляем их в новом местоположении при каждой итерации цикла
         ship.reset()
         monsters.draw(window)
+        bullets.draw(window)
+
+        if lost > 5:
+            finish = True
 
         display.update()
+
     # цикл срабатывает каждую 0.05 секунд
     time.delay(50)

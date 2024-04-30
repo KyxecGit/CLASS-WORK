959399544157c6b49ff2d301a0157a26125f0dcd
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-04-30 17:36:52 +0600

    Update example.py

--- example.py
+++ example.py
@@ -4,9 +4,9 @@ question = [
 ('2 + 2 = 4 ?', 'Может быть', 'Однозачно', 'Для гениев'),
 ('Любишь маму ?', 'Да', 'Нет', 'Для гениев'),
 ('Кто ты из Гарри потера ?', 'Я не смотрю аниме', 'Хагрид', 'Для дотеров'),
-('Можешь посмотреть в завтрашний день ?', 'Не только лишь каждый', 'Нет', 'Для дотеров'),
-]
-conn = sqlite3.connect('quiz.sqlite')
+('Можешь посмотреть в завтрашний день ?', 'Не только лишь каждый', 'Нет', 'Для дотеров'),]
+
+conn = sqlite3.connect('quiz_info.sqlite')
 cursor = conn.cursor()
 cursor.execute('''
                 CREATE TABLE quiz_info (
@@ -15,3 +15,9 @@ cursor.execute('''
                 wrong VARCHAR,
                 quiz VARCHAR )       ''')
 conn.commit()
+cursor.executemany('''
+                INSERT INTO quiz_info (question, answer, wrong, quiz)
+                VALUES (?,?,?,?)''', question)
+conn.commit()
+cursor.close()
+conn.close()

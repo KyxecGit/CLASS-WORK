afb16d26120cfed188497685e29ba2cc40122bb1
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-04-30 17:27:39 +0600

    Update example.py

--- example.py
+++ example.py
@@ -1,69 +1,17 @@
-from PyQt5.QtWidgets import (
-    QApplication, QWidget,
-    QLineEdit, QPushButton,
-    QLabel, QVBoxLayout)
+import sqlite3
 
-app = QApplication([])
-
-window = QWidget()
-window.setWindowTitle('Калькулятор ИМТ')
-window.resize(400,200)
-
-result = QLabel('Введите ваши данные: ')
-result.setStyleSheet('''
-                        color: black;
-                        background-color: white;
-                        font-size: 30px;
-                        padding: 10px;
-                        border: 5px solid black;
-''')
-weight = QLineEdit()
-weight.setStyleSheet('''
-                        color: black;
-                        background-color: white;
-                        font-size: 20px;
-                        padding: 10px;
-                        border: 5px solid black;
-''')
-weight.setPlaceholderText('Введите ваш вес: ')
-height = QLineEdit()
-height.setStyleSheet('''
-                        color: black;
-                        background-color: white;
-                        font-size: 20px;
-                        padding: 10px;
-                        border: 5px solid black;
-''')
-height.setPlaceholderText('Введите ваш рост: ')
-button = QPushButton('Расчитать')
-button.setStyleSheet('''
-                        color: black;
-                        background-color: white;
-                        font-size: 20px;
-                        padding: 10px;
-                        border: 5px solid black;
-''')
-
-layout = QVBoxLayout()
-layout.addWidget(result)
-layout.addWidget(weight)
-layout.addWidget(height)
-layout.addWidget(button)
-
-def show_result():
-    ves = weight.text()
-    rost = height.text()
-    try:
-        res = int(ves) / float(rost)**2
-        if res < 25:
-            result.setText('Ваш вес в пределах нормы')
-        else:
-            result.setText('У вас избыточный вес')
-    except:
-        result.setText('Вам нужно ввести цифры!')
-
-button.clicked.connect(show_result)
-
-window.setLayout(layout)
-window.show()
-app.exec()
+question = [
+('2 + 2 = 4 ?', 'Может быть', 'Однозачно', 'Для гениев'),
+('Любишь маму ?', 'Да', 'Нет', 'Для гениев'),
+('Кто ты из Гарри потера ?', 'Я не смотрю аниме', 'Хагрид', 'Для дотеров'),
+('Можешь посмотреть в завтрашний день ?', 'Не только лишь каждый', 'Нет', 'Для дотеров'),
+]
+conn = sqlite3.connect('quiz.sqlite')
+cursor = conn.cursor()
+cursor.execute('''
+                CREATE TABLE quiz_info (
+                question VARCHAR,
+                answer VARCHAR,
+                wrong VARCHAR,
+                quiz VARCHAR )       ''')
+conn.commit()

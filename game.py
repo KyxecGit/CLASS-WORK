5bd57e30e44a863bf0e766c56265824d77532934
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-06 17:05:59 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,154 +1,29 @@
-from typing import Any
 from pygame import *
-from random import randint
 
-#фоновая музыка
-#mixer.init()
-#mixer.music.load('space.ogg')
-#mixer.music.play()
-#fire_sound = mixer.Sound('fire.ogg')
-
-#шрифты и надписи
-font.init()
-font2 = font.Font(None, 36)
-
-# нам нужны такие картинки:
-img_back = "galaxy.jpg" # фон игры
-img_hero = "rocket.png" # герой
-img_enemy = "ufo.png" # враг
-
-score = 0 # сбито кораблей
-lost = 0 # пропущено кораблей
-life = 3
-
-# класс-родитель для других спрайтов
-class GameSprite(sprite.Sprite):
-  # конструктор класса
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        # Вызываем конструктор класса (Sprite):
-        sprite.Sprite.__init__(self)
-
-        # каждый спрайт должен хранить свойство image - изображение
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
-
-        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+class GameSprite():
+    def __init__(self,x,y,img):
+        self.image = transform.scale(image.load(img),(50,50))
         self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
- 
-  # метод, отрисовывающий героя на окне
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        self.rect.x = x
+        self.rect.y = y
 
-# класс главного игрока
-class Player(GameSprite):
-    # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
-    def fire(self):
-        bullet = Bullet('bullet.png',self.rect.centerx,self.rect.top,15,20,20)
-        bullets.add(bullet)
+    def view(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
 
-class Bullet(GameSprite):
-    def update(self):
-        self.rect.y -= self.speed
+hero = GameSprite(100,400,'hero.png')
 
-# класс спрайта-врага   
-class Enemy(GameSprite):
-    # движение врага
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        # исчезает, если дойдет до края экрана
-        if self.rect.y > win_height:
-            self.rect.x = randint(80, win_width - 80)
-            self.rect.y = 0
-            lost = lost + 1
+window = display.set_mode((700,500))
+background = transform.scale(image.load('background.jpg'),(700,500))
 
-# Создаем окошко
-win_width = 700
-win_height = 500
-display.set_caption("Shooter")
-window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
+game = True
+while game:
 
-# создаем спрайты
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-
-bullets = sprite.Group()
-monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-    monsters.add(monster)
-
-# переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
-finish = False
-# Основной цикл игры:
-run = True # флаг сбрасывается кнопкой закрытия окна
-while run:
-    # событие нажатия на кнопку Закрыть
+    window.blit(background,(0,0))
+    
     for e in event.get():
         if e.type == QUIT:
-            run = False
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                #fire_sound.play()
-                ship.fire()
+            game = False
 
-    if not finish:
-        # обновляем фон
-        window.blit(background,(0,0))
-
-        # пишем текст на экране
-        text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
-        window.blit(text, (10, 20))
-
-        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 50))
-
-        text_life = font.Font(None,80).render(str(life), 1, (255, 255, 255))
-        window.blit(text_life, (600, 10))
-
-        # производим движения спрайтов
-        ship.update()
-        monsters.update()
-        bullets.update()
-
-        # обновляем их в новом местоположении при каждой итерации цикла
-        ship.reset()
-        monsters.draw(window)
-        bullets.draw(window)
-
-       
-        if sprite.spritecollide(ship,monsters,True):
-            life -= 1
-
-        if lost > 5 or life == 0:
-            finish = True
-            lose = font.Font(None,120).render('YOU LOSE',True,(255,0,0))
-            window.blit(lose,(200,200))
-
-        if sprite.groupcollide(bullets,monsters,True,True):
-            score += 1
-            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-            monsters.add(monster)
-            if score == 10:
-                finish = True
-                win = font.Font(None,120).render('YOU WIN',True,(0,255,0))
-                window.blit(win,(200,200))
-
-        display.update()
-
-    else:
-        finish = False
-        lost = 0
-        score = 0
-        time.delay(5000)
-    # цикл срабатывает каждую 0.05 секунд
-    time.delay(50)
+    hero.view()
+    
+    display.update()

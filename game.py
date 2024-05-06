fbf94e0d56902f5f7066aa6502dae73ca36ee968
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-06 17:54:36 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from pygame import *
 
+#Создаем класс для наших спрайтов
 class GameSprite():
     def __init__(self,x,y,img):
         self.image = transform.scale(image.load(img),(70,70))
@@ -7,25 +8,44 @@ class GameSprite():
         self.rect.x = x
         self.rect.y = y
 
+    #Способность отображение для персонажей
     def view(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
+class Wall():
+    def __init__(self,x,y,width,height):
+        self.image = Surface((width,height))
+        self.image.fill((0,255,0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def view(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+#Создаем обьекты для нашей игры
+wall_1 = Wall(50,50,600,20)
 hero = GameSprite(100,400,'hero.png')
 enemy = GameSprite(500,200,'cyborg.png')
 gold = GameSprite(600,400,'treasure.png')
-
+#Создание экрана
 window = display.set_mode((700,500))
+#Картинка для нашего экрана
 background = transform.scale(image.load('background.jpg'),(700,500))
 
+#Игровой цикл
 game = True
 while game:
 
     window.blit(background,(0,0))
     
+    #Обработка выхода из игры
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    #Движение главного героя
     keys = key.get_pressed()
     if keys[K_UP]:
         hero.rect.y -= 10
@@ -36,21 +56,13 @@ while game:
     if keys[K_RIGHT]:
         hero.rect.x += 10
 
-    
-    #if enemy.rect.x  <= 500:
-        #side = 'right'
-    #if enemy.rect.x >= 100:
-        #side = 'left'
-    
-    #if side == 'left':
-        #enemy.rect.x -= 10
-    #else:
-        #enemy.rect.x += 10
-
-
+    #Отображение персонажей
     hero.view()
     enemy.view()
     gold.view()
 
-    time.delay(50)
+    wall_1.view()
+    #Команда отвечает за частоту отработки цикла
+    time.delay(5)
+    #Обновление нашего экрана
     display.update()

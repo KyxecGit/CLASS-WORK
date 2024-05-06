46d1226c7ba8bfd0a7db2b2cb378496d5325661f
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-06 17:23:15 +0600

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from pygame import *
 
 class GameSprite():
     def __init__(self,x,y,img):
-        self.image = transform.scale(image.load(img),(50,50))
+        self.image = transform.scale(image.load(img),(70,70))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -11,6 +11,8 @@ class GameSprite():
         window.blit(self.image,(self.rect.x,self.rect.y))
 
 hero = GameSprite(100,400,'hero.png')
+enemy = GameSprite(500,200,'cyborg.png')
+gold = GameSprite(600,400,'treasure.png')
 
 window = display.set_mode((700,500))
 background = transform.scale(image.load('background.jpg'),(700,500))
@@ -24,6 +26,18 @@ while game:
         if e.type == QUIT:
             game = False
 
+    keys = key.get_pressed()
+    if keys[K_UP]:
+        hero.rect.y -= 10
+    if keys[K_DOWN]:
+        hero.rect.y += 10
+    if keys[K_LEFT]:
+        hero.rect.x -= 10
+    if keys[K_RIGHT]:
+        hero.rect.x += 10
+
     hero.view()
-    
+    enemy.view()
+    gold.view()
+
     display.update()

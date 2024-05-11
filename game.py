11d1d7c42ac02aeceb32d8bcb3698416169ac580
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-11 10:04:44 +0600

    Update game.py

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ class GameSprite():
         window.blit(self.image,(self.rect.x,self.rect.y))
 
 ball = GameSprite(350,200,'ball.png',50,50)
+player = GameSprite(0,200,'racket.png',75,150)
+ai = GameSprite(625,200,'racket.png',75,150)
 
 window = display.set_mode((700,500))
 
@@ -29,12 +31,20 @@ while game:
     ball.rect.x += speed_x
     ball.rect.y += speed_y
 
-    if ball.rect.x > 650 or ball.rect.x < 0:
+    if sprite.collide_rect(ball,player) or sprite.collide_rect(ball,ai):
         speed_x *= -1
-        
+
     if ball.rect.y > 450 or ball.rect.y < 0:
         speed_y *= -1
+
+    if ai.rect.y != ball.rect.y:
+        if ai.rect.centery < ball.rect.centery:
+            ai.rect.y += 2
+        else:
+            ai.rect.y -= 2
     
     ball.view()
+    player.view()
+    ai.view()
 
     display.update()

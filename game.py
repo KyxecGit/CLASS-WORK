25ae2d74c754ce34953b77984ac47b6127f7dedc
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-11 17:24:04 +0600

    Update game.py

--- game.py
+++ game.py
@@ -14,16 +14,27 @@ ball = GameSprite('ball.png',350,250,50,50)
 
 window = display.set_mode((700,500))
 
+speed_x = 1
+speed_y = 1
+
 game = True
 while game:
 
     window.fill((200,250,250))
+    ball.show()
     
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-    ball.show()
+   
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+    if ball.rect.x > 650 or ball.rect.x < 0:
+        speed_x *= -1
+    if ball.rect.y > 450 or ball.rect.y < 0:
+        speed_y *= -1
 
     
     display.update()
+    time.delay(2)

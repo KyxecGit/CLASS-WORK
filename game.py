3466c9a4913a44aa884e227453534042f7cb2cec
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-13 17:05:36 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,17 +1,30 @@
 from pygame import *
 
+class GameSprite():
+    def __init__(self,x,y,w,h,img):
+        self.image = transform.scale(image.load(img),(w,h))
+        self.hitbox = self.image.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+    
+    def show(self):
+        window.blit(self.image,(self.hitbox.x, self.hitbox.y))
+
 #Создаем экран
 window = display.set_mode( (700,500) )
 background =  transform.scale(image.load('back.jpg'),(700,500))
 
+hero = GameSprite(300,400,150,100,'hero.png')
 
 game = True
 while game:
+
+    window.blit(background,(0,0))
     
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-    window.blit(background,(0,0))
+    hero.show()
 
     display.update()

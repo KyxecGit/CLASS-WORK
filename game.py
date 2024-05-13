acac57fb44cba3bc0171a248e9a67f4d061e4d6f
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-13 22:49:51 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,44 +1,57 @@
 from pygame import *
 
-class GameSprite():
-    def __init__(self,x,y,w,h,img):
-        self.image = transform.scale(image.load(img),(w,h))
-        self.hitbox = self.image.get_rect()
-        self.hitbox.x = x
-        self.hitbox.y = y
-    
-    def show(self):
-        window.blit(self.image,(self.hitbox.x, self.hitbox.y))
+class GameSprite(sprite.Sprite):
+
+    def __init__(self, player_image, player_x, player_y, size_x, size_y):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
 
-    def move(self):
-        keys =  key.get_pressed()
-        if keys[K_LEFT] and self.hitbox.x > 0:
-            self.hitbox.x -= 5
-        if keys[K_RIGHT] and self.hitbox.x < 600:
-            self.hitbox.x += 5
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= 5
+        if keys[K_RIGHT] and self.rect.x < 620:
+            self.rect.x += 5
 
-        #движение с помощью мыши
-        #x,y = mouse.get_pos()
-        #self.hitbox.x = x
+    def fire(self):
+        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20)
+        bullets.add(bullet)
 
-#Создаем экран
-window = display.set_mode( (700,500) )
-background =  transform.scale(image.load('back.jpg'),(700,500))
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= 5
 
-hero = Player(300,400,100,100,'hero.png')
+window = display.set_mode((700, 500))
+background = transform.scale(image.load('galaxy.jpg'), (700, 500))
 
-game = True
-while game:
+ship = Player('rocket.png', 5, 400, 80, 100)
+bullets = sprite.Group()
 
-    window.blit(background,(0,0))
-    
+run = True 
+while run:
+
     for e in event.get():
         if e.type == QUIT:
-            game = False
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                ship.fire()
 
-    hero.show()
-    hero.move()
+    window.blit(background,(0,0))
+
+    ship.update()
+    bullets.update()
+
+    ship.reset()
+    bullets.draw(window)
 
     display.update()

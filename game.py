ba16fca7246d435382d48c11e4d19284d6c77d98
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-19 13:58:38 +0600

    Update game.py

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ pica = transform.scale(pica,(100,100))
 gitler = image.load('gitler.png')
 gitler = transform.scale(gitler,(100,100))
 
+gitler_x = 0
+gitler_y = 0
 
 game = True
 while game:
@@ -19,9 +21,20 @@ while game:
         if e.type == QUIT:
             game = False
 
+    keys = key.get_pressed()
+    if keys[K_UP]:
+        gitler_y -= 5
+    if keys[K_DOWN]:
+        gitler_y += 5
+    if keys[K_LEFT]:
+        gitler_x -= 5
+    if keys[K_RIGHT]:
+        gitler_x += 5
+
+
     window.blit(back,(0,0))
     window.blit(pica,(200,200))
-    window.blit(gitler,(400,200))
+    window.blit(gitler,(gitler_x,gitler_y))
     display.update()
 
     

306816c6b57d64e0238db956e30c89d0169e853b
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-20 17:28:27 +0600

    Update game.py

--- game.py
+++ game.py
@@ -11,14 +11,18 @@ class GameSprite():
         window.blit(self.image,(self.rect.x,self.rect.y))
 
 ball = GameSprite(350,200,50,50,'ball.png')
+gitler = GameSprite(620,200,80,150,'gitler.png')
+pica = GameSprite(0,200,80,150,'pica.png')
 
 window = display.set_mode((700,500))
+
 #музыка
 #mixer.init()
 #mixer.music.load('back.mp3')
 #mixer.music.play()
 
 speed_x = 1
+speed_y = 1
 
 game = True
 while game:
@@ -30,10 +34,18 @@ while game:
             game = False
 
     ball.show()
+    gitler.show()
+    pica.show()
+
+
     ball.rect.x += speed_x
+    ball.rect.y += speed_y
 
     if ball.rect.x > 650 or ball.rect.x < 0:
         speed_x *= -1
+
+    if ball.rect.y > 450 or ball.rect.y < 0:
+        speed_y *= -1
 
     time.delay(10)
     display.update()

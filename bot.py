4b8dd1ebaddd36baf041ec6cd7738458fce89a07
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-06-19 15:56:47 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,75 +1,57 @@
-from random import randint
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, QVBoxLayout, 
+        QGroupBox, QRadioButton,  
+        QPushButton, QLabel)
 
-print('Здравствуйте меня зовут Генадий сегодня я буду вашим ботом')
-print('1 - шутка 2 - рекомендация китайских мультиков')
-print('3 - игра угадай число 4 - купить раба')
+#ИНТЕРФЕЙС
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('Memo Card')
+window.resize(400, 300)
 
-otvet = input('Что вас интересует?').lower()
+btn_OK = QPushButton('Ответить') # кнопка ответа
+lb_Question = QLabel('Самый сложный вопрос в мире!') # текст вопроса
+#ФОРМА С ВАРИАНТАМИ ОТВЕТОВ
+RadioGroupBox = QGroupBox("Варианты ответов") # группа на экране для переключателей с ответами
 
-while otvet.find('дос') == -1:
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
 
-    if otvet.find('шут') >= 0 :
-        print('Колобок повесился')
-    elif otvet.find('ани') >= 0:
-        otvet = input('Какой жанр вас интересует?')
-        if otvet == 'комедия':
-            print('ван панч мен \ ханакун \ волейбол')
-        elif otvet == 'исекай':
-            print('реинкарнация бомжика \ поднятия уровня \ какая то слизь ')
-        else:
-            print('Я не знаю такой жанр могу порекомендовать вам Атаку Титаника')
-    elif otvet.find('игр') >= 0:
-        print('Я загадал число от 1 до 100 посмотрим с какой попытки ты его угадаешь?)')
-        rand_num = randint(0,100)
-        otvet = int(input('Введите число:'))
-        for popytka in range(10): 
+layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans2.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans2.addWidget(rbtn_4)
 
-            if otvet == rand_num:
-                print('Приз пачка сухариков')
-                break
-            else:
-                if otvet > rand_num:
-                    print('Загадоное число меньше')
-                else:
-                    print('Загадоное число больше')
+RadioGroupBox.setLayout(layout_ans2) # готова "панель" с вариантами ответов 
 
-            otvet = int(input('Попробуй еще раз'))
-        print('Вы угадали с',popytka + 1,'попытки')
-    elif otvet.find('куп') >= 0:
-        print('Добро пожаловать в Черно-Шоп')
-        print('1 - Аркадий (100$) 2 - Виталя (200$) 3 - Лейкопластырь (1$)')
-        money = int(input('Сколько у вас деняк?'))
-        tovar = input('Что бы вы хотели приобрести?')
-        if tovar == '1' and money >= 100:
-            print('Вы купили Аркашу за 100 долларов')
-            print('У вас осталось', money - 100)
-        elif tovar == '2' and money >= 200:
-            print('Вы купили Виталика за 200 долларов')
-            print('У вас осталось', money - 200)
-        elif tovar == '3':
-            print('Вы купили лейкопластырь за бакс')
-            print('У вас осталось', money - 1)
-        else:
-            print('У вас недостаточно деняк')    
-    else:
-        print('Я тоби не понимаю')
+#ФОРМА С РЕЗУЛЬТАТОМ
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
+lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
 
-    otvet = input('Что нибудь еще?').lower()
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result)
+layout_res.addWidget(lb_Correct)
+AnsGroupBox.setLayout(layout_res)
 
-print('Всего хорошего!!!')
+AnsGroupBox.hide() # скроем панель с ответом, сначала должна быть видна панель вопросов
 
+#РАЗМЕЩЕНИЕ ВИДЖЕТОВ
+layout_card = QVBoxLayout()
 
+layout_card.addWidget(lb_Question)
+layout_card.addWidget(RadioGroupBox)
+layout_card.addWidget(AnsGroupBox)
+layout_card.addWidget(btn_OK)
+#ФУНКЦИОНАЛ
 
+#ПОДПИСКИ
 
-
-
-
-
-
-
-
-
-
-
-
-
+#ЗАПУСК
+window.setLayout(layout_card)
+window.show()
+app.exec()
